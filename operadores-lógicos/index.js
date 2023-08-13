498fa8e36a63336536e11506875065d86623b4f9
Gileade <gileadelima16@gmail.com>
2023-08-13 10:54:08 -0300

    finalizando operadores-logico

---
+++ operadores-lógicos/index.js
@@ -0,0 +1,16 @@
+/*
+Operadores Lógicos
+&& -> AND -> E
+|| -> OR -> OU
+! -> NOT -> NÃO
+
+*/
+let a = 2
+let b = 4
+
+console.log(a && b > 5)
+
+console.log(a || b > 5)
+
+
+console.log(!false) // Inverte os valores, true vira false e vice versa ( negação )
