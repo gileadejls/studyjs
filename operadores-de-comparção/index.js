ea261a26c65e06a562db294df50032ca5a5caa8c
Gileade <gileadelima16@gmail.com>
2023-08-13 10:43:16 -0300

    finalizando aula

---
+++ operadores-de-comparção/index.js
@@ -0,0 +1,14 @@
+/*
+Operadores de comparação
+> Maior que
+>= Maior que ou igual a
+< Menor que
+<= Menor que ou igual a
+== igualdade / valor ( = atribuição )
+=== igualdade restrita ( valor e tipo )
+!= diferente
+!== diferente estrito 
+
+*/
+
+console.log(4 != '4')
